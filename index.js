9c5b9ba8e5c4c6fc37b68713766286e7d2ff6d1a
KanakaNakazawa <97468385+KanakaNakazawa@users.noreply.github.com>
2022-05-26 07:07:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const { EventEmitter } = require("node:events")
+const { WebSocket } = require('./src/core/WebSocket.js')
+class Client extends EventEmitter {
+  constructor(options = {}) {
+    super()
+    this.token = options.token || null
+    this.intents = options.intents || null
+  }
 
+  login(token) {
+    if(this?.token === null) {
+      if(token === undefined) throw new Error("Token Tidak Ada")
+    }
+    if (this?.intents === undefined) throw new Error("Intents Harus Terisi")
+    const websocket = new WebSocket(client)
+    websocket.start()
+  }
+}
