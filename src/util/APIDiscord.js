89d4e9c454eb05c1d1be54fc94a80070510a15a0
KanakaNakazawa <97468385+KanakaNakazawa@users.noreply.github.com>
2022-05-29 10:38:21 +0700

    Create APIDiscord.js

---
+++ src/util/APIDiscord.js
@@ -0,0 +1,11 @@
+function APIDiscord(method = "", params = "", token = "", data) {
+  const object = {
+    headers: []
+  }
+  object.headers["Authorization"] = `Bot ${token}`
+  if(data) object["data"] = data
+  
+  fetch("https://discord.com/api/v10" + params, object)
+}
+
+module.exports = APIDiscord
