b863c5c697efd8c4ae3effdfec662c6b7884c6ae
KanakaNakazawa <97468385+KanakaNakazawa@users.noreply.github.com>
2022-05-29 10:37:14 +0700

    Create BaseInteraction.js

---
+++ src/structure/BaseInteraction.js
@@ -0,0 +1,43 @@
+class BaseInteraction {
+  constructor(options = {}) {
+    this.token = null
+    this.rawdata = null
+  }
+  
+  isChatInputCommand() {
+    if(this.rawdata.type === 2) {
+      if(this.rawdata.data.type === 1) return true
+    }
+    return false
+  }
+  
+  isUserCommand() {
+    if (this.rawdata.type === 2) {
+      if (this.rawdata.data.type === 2) return true
+    }
+    return false
+  }
+  
+  isMessageCommand() {
+    if(this.rawdata.type === 2) {
+      if(this.rawdata.data.type === 3) return true
+    }
+    return false
+  }
+  
+  isButton() {
+    if (this.rawdata.type === 3) {
+      if(this.rawdata.data.type === 2) return true
+    }
+    return false
+  }
+  
+  isSelectMenu() {
+    if (this.rawdata.type === 3) {
+      if (this.rawdata.data.type === 3) return true
+    }
+    return false
+  }
+}
+
+module.exports = BaseInteraction
