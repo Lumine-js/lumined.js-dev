c4e3edef127a9970c05e5b046b273a28889fd61c
KanakaNakazawa <97468385+KanakaNakazawa@users.noreply.github.com>
2022-07-02 15:17:28 +0700

    Create UserClient.js

---
+++ src/structure/UserClient.js
@@ -0,0 +1,25 @@
+const Base = require("./Base.js")
+class UserClient extends Base {
+  constructor(options = {}, client, unchached) {
+    this.id = options.user.id
+    this.username = options.user.username
+    this.discriminator = options.user.discriminator
+    this.avatar = options.user.avatar
+    this.banner = options.user.banner
+    this.bot = options.user.bot
+    this.accentColor = options.user.accent_color
+    this.recache = "user"
+  }
+  
+  /*displayAvatarURL(options = {}) {
+    if(!options?.size) options.size = 256
+    if(!options?.static) options.static = true
+    return ""
+  }
+  
+  displayBannerURL(options = {}) {
+    
+  }*/
+}
+
+module.exports = UserClient
