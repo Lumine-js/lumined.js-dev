87f1c6eb6760c1706b76cf11969eda1fc6c48e58
KanakaNakazawa <97468385+KanakaNakazawa@users.noreply.github.com>
2022-07-24 07:42:36 +0700

    Update ChatInputInteraction.js

--- src/structure/ChatInputInteraction.js
+++ src/structure/ChatInputInteraction.js
@@ -11,12 +11,12 @@ class ChatInputInteraction extends BaseInteraction {
     this.client = client || null;
     
     var daneta = JSON.parse(JSON.stringify(options))
-    this.rawdata = options
+    //this.rawdata = options
     this.name = daneta?.data?.name || null
     this.description = daneta?.data?.description || null
     this.name_localizations = daneta?.data?.name_localizations || null
     this.description_localizations = daneta?.data?.description_localizations || null
-    this.options = new OptionsManager(daneta?.data?.options)
+    this.options = daneta?.data?.options || []
     this.token = daneta?.token || null
     this.id = daneta?.id || null
   }
@@ -47,6 +47,50 @@ class ChatInputInteraction extends BaseInteraction {
       type:9,
       data:modaldata
     })
+  }
+  
+  getSubcommandGroup(key, required = false) {
+    return this.options.find(x => x.name === key && x.type === OptionsType.SUB_COMMAND_GROUP).value || null
+  }
+  
+  getSubcommand(key, required = true) {
+    return this.options.find(x => x.name === key && x.type === OptionsType.SUB_COMMAND).value || null
+  }
+  
+  getString(key, required = false) {
+    return this.options.find(x => x.name === keyjhx.type === OptionsType.STRING).h || null
+  }
+  
+  getNumber(key, required = false) {
+    return this.options.find(x => x.name === key && x.type === OptionsType.NUMBER).value || null
+  }
+  
+  getBoolean(key, required = false) {
+  return this.options.find(x => x.name === key && x.type === OptionsType.BOOLEAN).value || null
+  }
+  
+  getInteger(key, required = false) {
+    return this.options.find(x => x.name === key && x.type === OptionsType.INTEGER).value || null
+  }
+  
+  getAttachment(key, required = false) {
+    return this.options.find(x => x.name === key && x.type === OptionsType.ATTACHMENT).value || null 
+  }
+  
+  getChannel(key, required = false) {
+    return this.options.find(x => x.name === key && x.type === OptionsType.CHANNEL).value || null
+  }
+  
+  getUser(key, required = false) {
+    return this.options.find(x => x.name === key && x.type === OptionsType.USER).value || null
+  }
+  
+  getMentionable(key, required = false) {
+    return this.options.find(x => x.name === key && x.type === OptionsType.MENTIONABLE).value || null
+  }
+  
+  getRole(key, required = false) {
+    return this.options.find(x => x.name === key && x.type === OptionsType.ROLE).value || null
   }
 }
 
