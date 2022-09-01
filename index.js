9a716eb5883e30bca462ab5c00c86cc82abcf1b1
KanakaNakazawa <97468385+KanakaNakazawa@users.noreply.github.com>
2022-09-01 20:11:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 const Client = require("./src/client/Client.js")
 
 //Structure Constructor
-const Embed = require('./src/structure/Embed.js')
-const ActionRow = require('./src/structure/ActionRow.js')
-const Button = require('./src/structure/Button.js')
-const Command = require('./src/structure/Command.js')
-const TextInput = require('./src/structure/TextInput.js')
-const SelectMenu = require('./src/structure/SelectMenu.js')
-const Modal = require('./src/structure/Modal.js')
+const { Embed } = require('./src/structure/Embed.js')
+const { ActionRow } = require('./src/structure/ActionRow.js')
+const { Button } = require('./src/structure/Button.js')
+const { Command } = require('./src/structure/Command.js')
+const { TextInput } = require('./src/structure/TextInput.js')
+const { SelectMenu } = require('./src/structure/SelectMenu.js')
+const { Modal } = require('./src/structure/Modal.js')
 
 //Util
 const Constants = require('./src/util/constants.js')
