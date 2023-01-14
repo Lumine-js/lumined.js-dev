43d1d63165f5bf37100b7a4a8ab9d73fee1d76a4
KanakaNakazawa <97468385+KanakaNakazawa@users.noreply.github.com>
2023-01-14 20:50:32 +0700

    Update ActionRow.js

--- src/structure/ActionRow.js
+++ src/structure/ActionRow.js
@@ -1,35 +1,35 @@
 class ActionRow {
   constructor(data) {
     this.type = 1
-    this.components = data?.components || null
+    this.components = data.components ? data.components : null
   }
 
   addComponent(obj) {
-    if(this.components === null) this.components = []
+    if (this.components === null) this.components = []
     if (!obj) {
-      console.log('Obj Cannot Empty')
+      throw new Error('Obj Cannot Empty')
       return this
     }
     if (!typeof obj === "object") {
-      console.log("Obj Must Be A Object From Component Builder")
+      throw new Error("Obj Must Be A Object From Component Builder")
       return this
     }
     this.components.push(obj)
   }
 
   addComponents(naray) {
-    if(this.components === null) this.components = []
+    if (this.components === null) this.components = []
     if (!typeof naray === "array") {
-      console.log('Naray Must Be Array')
+      throw new Error('Naray Must Be Array')
       return this
     }
     naray.map((yanto, yanti) => {
       if (!yanto) {
-        console.log(`[Component ${yanti}] ` + 'Obj Cannot Empty')
+        throw new Error(`[Component ${yanti}] ` + 'Obj Cannot Empty')
         return this
       }
       if (!typeof yanto === "object") {
-        console.log(`[Component ${yanti}] ` + "Obj Must Be A Object From Component Builder")
+        throw new Error(`[Component ${yanti}] ` + "Obj Must Be A Object From Component Builder")
         return this
       }
       this.components.push(yanto)
