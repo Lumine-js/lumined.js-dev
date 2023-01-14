2d5df02fec6920ad792459e7687c9966c781c8a9
KanakaNakazawa <97468385+KanakaNakazawa@users.noreply.github.com>
2023-01-14 20:54:30 +0700

    Update Embed.js

--- src/structure/Embed.js
+++ src/structure/Embed.js
@@ -1,41 +1,41 @@
-const { ResolveColor } = require("./../util/constants.js")
+const { ResolveColor } = require("./../util/Constants.js")
 
 class Embed {
   constructor(raw) {
-    this.title = raw?.title || null
-    this.description = raw?.description || null
-    this.url = raw?.url || null
-    this.fields = raw?.fields || []
-    this.author = raw?.author || null
-    this.footer = raw?.footer || null
-    this.image = raw?.image || null
-    this.color = raw?.color || ResolveColor("Random")
+    this.title = raw.title ? raw.title : null
+    this.description = raw.description ? raw.description : null
+    this.url = raw.url ? raw.url : null
+    this.fields = raw.fields ? raw.fields : []
+    this.author = raw.author ? raw.author : null
+    this.footer = raw.footer ? raw.footer : null
+    this.image = raw.image ? raw.image : null
+    this.color = raw.color ? raw.color : ResolveColor("Random")
   }
 
   addField(name, value, inline) {
     try {
       if (!name) {
-        console.log("(Builder Alert) Parameter name Must Fill!")
+        throw new Error("Parameter name Must Fill!")
         return this
       }
       if (!typeof name === "string") {
-        console.log("(Builder Alert) Parameter name Must Be A String")
+        throw new Error("Parameter name Must Be A String")
         return this
       }
       if (name.length === 0) {
-        console.log("(Builder Alert) Parameter name Cannot Be Empty")
+        throw new Error("Parameter name Cannot Be Empty")
         return this
       }
       if (!value) {
-        console.log("(Builder Alert) Parameter value Must Fill!")
+        throw new Error("Parameter value Must Fill!")
         return this
       }
       if (!typeof value === "string") {
-        console.log("(Builder Alert) Parameter value Must Be A String")
+        throw new Error("Parameter value Must Be A String")
         return this
       }
       if (value.length === 0) {
-        console.log("(Builder Alert) Parameter value Cannot Be Empty")
+        throw new Error("Parameter value Cannot Be Empty")
         return this
       }
       if (!inline || (!typeof inline === "boolean")) {
@@ -45,8 +45,8 @@ class Embed {
       if (!Array.isArray(this.fields)) this.fields = []
       this.fields.push({ name: name, value: value, inline: inline })
       return this
-    } catch(err) {
-      console.log("(Builder Error) " + err)
+    } catch (err) {
+      throw new Error(err)
       return this
     }
   }
@@ -54,11 +54,11 @@ class Embed {
   addFields(fields) {
     try {
       if (!Array.isArray(fields)) {
-        console.log("(Builder Alert) Fields Must be array")
+        throw new Error("Fields Must be array")
         return this
       }
       if (fields.length === 0) {
-        console.log("(Builder Alert) Fields Cannot Be Empty")
+        throw new Error("Fields Cannot Be Empty")
         return this
       }
 
@@ -96,8 +96,8 @@ class Embed {
         this?.fields.push({ title: neh?.name, value: neh?.value, inline: neh?.inline })
       })
       return this
-    } catch(err) {
-      console.log("(Builder Error) " + err)
+    } catch (err) {
+      throw new Error(err)
       return this
     }
   }
@@ -114,8 +114,8 @@ class Embed {
         this.color = [color[0], color[1], color[2]]
         return this
       }
-    } catch(err) {
-      console.log("(Builder Error) " + err)
+    } catch (err) {
+      throw new Error(err)
       return this
     }
   }
@@ -123,31 +123,31 @@ class Embed {
   setImage(image) {
     try {
       if (typeof image === "string") {
-        this.image = {url: image}
+        this.image = { url: image }
         return this
       } else {
         if (!image?.url) {
-          console.log("(Builder Alert) Parameter image.url Must Fill!")
+          throw new Error("Parameter image.url Must Fill!")
           return this
         }
         if (!typeof image?.url === "string") {
-          console.log("(Builder Alert) Parameter image.url Must Be A String")
+          throw new Error("Parameter image.url Must Be A String")
           return this
         }
         if (image?.url.length === 0) {
-          console.log("(Builder Alert) Parameter image.url Cannot Be Empty")
+          throw new Error("Parameter image.url Cannot Be Empty")
           return this
         }
         if ((!image.url?.startsWith("http")) || (!image.url?.startsWith("https"))) {
-          console.log("(Builder Alert) Parameter image.url Must Be Using URL\n\nLike \"https://google.com/image.jpg\"")
+          throw new Error("Parameter image.url Must Be Using URL\n\nLike \"https://google.com/image.jpg\"")
           return this
         }
         this.image = { url: image.url }
         return this
 
       }
-    } catch(err) {
-      console.log("(Builder Error) " + err)
+    } catch (err) {
+      throw new Error(err)
       return this
     }
   }
@@ -155,25 +155,25 @@ class Embed {
   setThumbnail(image) {
     try {
       if (!image?.url) {
-        console.log("(Builder Alert) Parameter image.url Must Fill!")
+        throw new Error("Parameter image.url Must Fill!")
         return this
       }
       if (!typeof image?.url === "string") {
-        console.log("(Builder Alert) Parameter image.url Must Be A String")
+        throw new Error("Parameter image.url Must Be A String")
         return this
       }
       if (image?.url.length === 0) {
-        console.log("(Builder Alert) Parameter image.url Cannot Be Empty")
+        throw new Error("Parameter image.url Cannot Be Empty")
         return this
       }
       if ((!image.url?.startsWith("http")) || (!image.url?.startsWith("https"))) {
-        console.log("(Builder Alert) Parameter image.url Must Be Using URL\n\nLike \"https://google.com/image.jpg\"")
+        throw new Error("Parameter image.url Must Be Using URL\n\nLike \"https://google.com/image.jpg\"")
         return this
       }
       this.thumbnail = { url: image.url }
       return this
-    } catch(err) {
-      console.log("(Builder Error) " + err)
+    } catch (err) {
+      throw new Error(err)
       return this
     }
   }
@@ -181,21 +181,21 @@ class Embed {
   setDescription(description) {
     try {
       if (!description) {
-        console.log("(Builder Alert) Parameter description Must Fill!")
+        throw new Error("Parameter description Must Fill!")
         return this
       }
       if (!typeof description === "string") {
-        console.log("(Builder Alert) Parameter description Must Be A String")
+        throw new Error("Parameter description Must Be A String")
         return this
       }
       if (description.length === 0) {
-        console.log("(Builder Alert) Parameter description Cannot Be Empty")
+        throw new Error("Parameter description Cannot Be Empty")
         return this
       }
       this.description = description
       return this
-    } catch(err) {
-      console.log("(Builder Error) " + err)
+    } catch (err) {
+      throw new Error(err)
       return this
     }
   }
@@ -203,21 +203,21 @@ class Embed {
   setTitle(title) {
     try {
       if (!title) {
-        console.log("(Builder Alert) Parameter title Must Fill!")
+        throw new Error("Parameter title Must Fill!")
         return this
       }
       if (!typeof title === "string") {
-        console.log("(Builder Alert) Parameter title Must Be A String")
+        throw new Error("Parameter title Must Be A String")
         return this
       }
       if (title.length === 0) {
-        console.log("(Builder Alert) Parameter title Cannot Be Empty")
+        throw new Error("Parameter title Cannot Be Empty")
         return this
       }
       this.title = title
       return this
-    } catch(err) {
-      console.log("(Builder Error) " + err)
+    } catch (err) {
+      throw new Error(err)
       return this
     }
   }
@@ -226,15 +226,15 @@ class Embed {
     try {
       if ((!name?.name) && (!name?.iconURL) && (!name?.url)) {
         if (!name) {
-          console.log("(Builder Alert) Parameter name Must Fill!")
+          throw new Error("Parameter name Must Fill!")
           return this
         }
         if (!typeof name === "string") {
-          console.log("(Builder Alert) Parameter name Must Be A String")
+          throw new Error("Parameter name Must Be A String")
           return this
         }
         if (name.length === 0) {
-          console.log("(Builder Alert) Parameter name Cannot Be Empty")
+          throw new Error("Parameter name Cannot Be Empty")
           return this
         }
         if (!iconURL) {
@@ -242,11 +242,11 @@ class Embed {
         }
         if (!typeof iconURL === "null") {
           if (!typeof iconURL === "string") {
-            console.log("(Builder Alert) Parameter iconURL Must Be A String")
+            throw new Error("Parameter iconURL Must Be A String")
             return this
           }
           if (iconURL.length === 0) {
-            console.log("(Builder Alert) Parameter iconURL Cannot Be Empty")
+            throw new Error("Parameter iconURL Cannot Be Empty")
             return this
           }
         }
@@ -257,11 +257,11 @@ class Embed {
         }
         if (!typeof url === "null") {
           if (!typeof url === "string") {
-            console.log("(Builder Alert) Parameter url Must Be A String")
+            throw new Error("Parameter url Must Be A String")
             return this
           }
           if (url.length === 0) {
-            console.log("(Builder Alert) Parameter url Cannot Be Empty")
+            throw new Error("Parameter url Cannot Be Empty")
             return this
           }
         }
@@ -269,15 +269,15 @@ class Embed {
         return this
       } else {
         if (!name?.name) {
-          console.log("(Builder Alert) Parameter name Must Fill!")
+          throw new Error("Parameter name Must Fill!")
           return this
         }
         if (!typeof name?.name === "string") {
-          console.log("(Builder Alert) Parameter name Must Be A String")
+          throw new Error("Parameter name Must Be A String")
           return this
         }
         if (name?.name.length === 0) {
-          console.log("(Builder Alert) Parameter name Cannot Be Empty")
+          throw new Error("Parameter name Cannot Be Empty")
           return this
         }
         if (!name?.iconURL) {
@@ -285,11 +285,11 @@ class Embed {
         }
         if (!typeof name?.iconURL === "null") {
           if ((!typeof name?.iconURL === "string")) {
-            console.log("(Builder Alert) Parameter iconURL Must Be A String")
+            throw new Error("Parameter iconURL Must Be A String")
             return this
           }
           if (name?.iconURL.length === 0) {
-            console.log("(Builder Alert) Parameter iconURL Cannot Be Empty")
+            throw new Error("Parameter iconURL Cannot Be Empty")
             return this
           }
         }
@@ -298,19 +298,19 @@ class Embed {
         }
         if (!typeof name?.iconURL === "null") {
           if ((!typeof name?.url === "string")) {
-            console.log("(Builder Alert) Parameter url Must Be A String")
+            throw new Error("Parameter url Must Be A String")
             return this
           }
           if (name?.url.length === 0) {
-            console.log("(Builder Alert) Parameter url Cannot Be Empty")
+            throw new Error("Parameter url Cannot Be Empty")
             return this
           }
         }
         this.author = { name: name.name, iconURL: name.iconURL, url: name.url }
         return this
       }
-    } catch(err) {
-      console.log("(Builder Error) " + err)
+    } catch (err) {
+      throw new Error(err)
       return this
     }
   }
@@ -319,15 +319,15 @@ class Embed {
     try {
       if ((!text?.text) && (!text?.iconURL)) {
         if (!text) {
-          console.log("(Builder Alert) Parameter text Must Fill!")
+          throw new Error("Parameter text Must Fill!")
           return this
         }
         if (!typeof text === "string") {
-          console.log("(Builder Alert) Parameter text Must Be A String")
+          throw new Error("Parameter text Must Be A String")
           return this
         }
         if (text.length === 0) {
-          console.log("(Builder Alert) Parameter text Cannot Be Empty")
+          throw new Error("Parameter text Cannot Be Empty")
           return this
         }
         if (!iconURL) {
@@ -335,11 +335,11 @@ class Embed {
         }
         if (!typeof iconURL === "null") {
           if ((!typeof iconURL === "string")) {
-            console.log("(Builder Alert) Parameter iconURL Must Be A String")
+            throw new Error("Parameter iconURL Must Be A String")
             return this
           }
           if (iconURL.length === 0) {
-            console.log("(Builder Alert) Parameter iconURL Cannot Be Empty")
+            throw new Error("Parameter iconURL Cannot Be Empty")
             return this
           }
         }
@@ -347,15 +347,15 @@ class Embed {
         return this
       } else {
         if (!text?.text) {
-          console.log("(Builder Alert) Parameter text Must Fill!")
+          throw new Error("Parameter text Must Fill!")
           return this
         }
         if (!typeof text?.text === "string") {
-          console.log("(Builder Alert) Parameter text Must Be A String")
+          throw new Error("Parameter text Must Be A String")
           return this
         }
         if (text?.text.length === 0) {
-          console.log("(Builder Alert) Parameter text Cannot Be Empty")
+          throw new Error("Parameter text Cannot Be Empty")
           return this
         }
         if (!text?.iconURL) {
@@ -363,19 +363,19 @@ class Embed {
         }
         if (!typeof text?.iconURL === "null") {
           if ((!typeof text?.iconURL === "string")) {
-            console.log("(Builder Alert) Parameter iconURL Must Be A String")
+            throw new Error("Parameter iconURL Must Be A String")
             return this
           }
           if (text?.iconURL.length === 0) {
-            console.log("(Builder Alert) Parameter iconURL Cannot Be Empty")
+            throw new Error("Parameter iconURL Cannot Be Empty")
             return this
           }
         }
         this.footer = { text: text.text, iconURL: text.iconURL }
         return this
       }
-    } catch(err) {
-      console.log("(Builder Error) " + err)
+    } catch (err) {
+      throw new Error(err)
       return this
     }
   }
@@ -383,21 +383,21 @@ class Embed {
   setURL(url) {
     try {
       if (!url) {
-        console.log("(Builder Alert) Parameter url Must Fill!")
+        throw new Error("Parameter url Must Fill!")
         return this
       }
       if (!typeof url === "string") {
-        console.log("(Builder Alert) Parameter url Must Be A String")
+        throw new Error("Parameter url Must Be A String")
         return this
       }
       if (url.length === 0) {
-        console.log("(Builder Alert) Parameter url Cannot Be Empty")
+        throw new Error("Parameter url Cannot Be Empty")
         return this
       }
       this.url = url
       return this
-    } catch(err) {
-      console.log("(Builder Error) " + err)
+    } catch (err) {
+      throw new Error(err)
       return this
     }
   }
@@ -409,13 +409,13 @@ class Embed {
         return this
       }
       if (!typeof date === "date") {
-        console.log("(Builder Alert) Parameter date Must Be A Date")
+        throw new Error("Parameter date Must Be A Date")
         return this
       }
       this.date = date
       return this
-    } catch(err) {
-      console.log("(Builder Error) " + err)
+    } catch (err) {
+      throw new Error(err)
       return this
     }
   }
