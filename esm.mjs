579a485318b13f784639b33028d12b0acc3a67b1
KanakaNakazawa <97468385+KanakaNakazawa@users.noreply.github.com>
2023-01-28 17:57:48 +0700

    Update esm.mjs

--- esm.mjs
+++ esm.mjs
@@ -1,8 +1,15 @@
 import luminedjs from "./index.js";
 
 const Resolver = {
-
+  CommandOptionType: Constants.CommandOptionType,
+  CommandPermissionType: Constants.CommandPermissionType,
+  CommandType: Constants.CommandType,
+  ButtonStyle: Constants.ButtonStyle,
+  TextInputStyle: Constants.TextInputStyle,
+  ResolveColor: Constants.ResolveColor,
+  ActivityType: Constants.ActivityType
 }
+
 export const {
   Client,
   Collection
