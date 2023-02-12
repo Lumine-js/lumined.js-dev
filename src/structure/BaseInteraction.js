9c843e52a0d5703c332174eed59ec7e7f550f590
tgigroupid@gmail.com <tgigroupid@gmail.com>
2023-02-12 21:42:12 +0700

    Webhook test

--- src/structure/BaseInteraction.js
+++ src/structure/BaseInteraction.js
@@ -1,44 +1,44 @@
 class BaseInteraction {
   constructor(options = {}) {
     this.token = null
-    this.rawdata = null
+    this?.rawdata = null
   }
   
   isCommand() {
-    if(this.rawdata.type === 2) return true
+    if(this?.rawdata?.type === 2) return true
   }
   
   isChatInputCommand() {
-    if(this.rawdata.type === 2) {
-      if(this.rawdata.data.type === 1) return true
+    if(this?.rawdata?.type === 2) {
+      if(this?.rawdata?.data?.type === 1) return true
     }
     return false
   }
   
   isUserCommand() {
-    if (this.rawdata.type === 2) {
-      if (this.rawdata.data.type === 2) return true
+    if (this?.rawdata?.type === 2) {
+      if (this?.rawdata?.data?.type === 2) return true
     }
     return false
   }
   
   isMessageCommand() {
-    if(this.rawdata.type === 2) {
-      if(this.rawdata.data.type === 3) return true
+    if(this?.rawdata?.type === 2) {
+      if(this?.rawdata?.data?.type === 3) return true
     }
     return false
   }
   
   isButton() {
-    if (this.rawdata.type === 3) {
-      if(this.rawdata.data.type === 2) return true
+    if (this?.rawdata?.type === 3) {
+      if(this?.rawdata?.data?.type === 2) return true
     }
     return false
   }
   
   isSelectMenu() {
-    if (this.rawdata.type === 3) {
-      if (this.rawdata.data.type === 3) return true
+    if (this?.rawdata?.type === 3) {
+      if (this?.rawdata?.data?.type === 3) return true
     }
     return false
   }
